c2ee77aa1ef88ca302c4fbf57305d23fef179e19
Abdulrahman Al Khalaf <aalkhala@smail.uni-koeln.de>
2025-12-03 14:20:27 +0100

    first

---
+++ src/p1/Bruch.java
@@ -0,0 +1,7 @@
+package p1;
+
+public class Bruch {
+    public int z;
+    public int n;
+
+}
---
+++ src/p1/P1_main.java
@@ -0,0 +1,9 @@
+package p1;
+public class P1_main {
+    public static void main(String[] args) {
+        multiFrac(3, 6, 7, 8);
+    }
+    public static void multiFrac(int z1, int n1, int z2, int n2){
+        System.out.println(z1 * z2 + " : " + n1 * n2);
+    }
+}
